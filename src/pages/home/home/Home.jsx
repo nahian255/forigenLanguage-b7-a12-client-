efc6c0a73ba5c9a9b83821675612a33ce3fecd37
nahian255 <nahimn253@gmail.com>
2023-06-09 19:51:39 +0600

    popular ins

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,26 +1,11 @@
-// import { useQuery } from '@tanstack/react-query';
-// import useAxiosSecure from '../../../hooks/useAxiosSecure';
+
 import Banner from '../banner/Banner';
+import PopularInst from '../popularInst/PopularInst';
 import Slider from '../Top slider/Slider';
 
 
 const Home = () => {
 
-    // const [axiosSecure] = useAxiosSecure()
-
-    // axios.get('http://localhost:3000/first')
-    //     .then(data => console.log(data.data))
-
-    // const { data: room = [] } = useQuery({
-    //     queryKey: ['first'],
-    //     queryFn: async () => {
-    //         const res = await axiosSecure.get('/first')
-    //         // console.log(res.data);
-    //         return res.data
-    //     }
-    // })
-
-    // axiosSecure.get('/first').then(data => console.log(data.data))
 
     return (
         <div>
@@ -28,6 +13,8 @@ const Home = () => {
             <div className='my-24 '>
                 <Slider></Slider>
             </div>
+            <PopularInst></PopularInst>
+
         </div>
     );
 };
---
+++ src/pages/home/popularInst/PopularInst.jsx
@@ -0,0 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import InstructorCard from '../../Instructors/InstructorCard';
+
+const PopularInst = () => {
+    const [axiosSecure] = useAxiosSecure()
+    const { data: users = [], } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/users/instructor')
+            return res.data
+        }
+    });
+    const newUsers = users.slice(0, 6)
+    console.log(newUsers);
+
+    return (
+        <div>
+            <h1 className="text-4xl text-center">See our best Instructor..</h1>
+            <div className='flex flex-row flex-wrap gap-14'>
+                {
+                    newUsers?.map(item => <InstructorCard key={item?._id} item={item}></InstructorCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default PopularInst;
